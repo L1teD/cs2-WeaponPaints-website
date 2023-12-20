7c00d78e1336df83f993b3efbc80fb12fe674ae7
L1teD <l1tedane4ek@gmail.com>
2023-12-20 17:58:17 +0900

    fixed bayonet, reworked showDefaults func

--- src/public/js/sideBtns.js
+++ src/public/js/sideBtns.js
@@ -3,99 +3,21 @@ const sideBtnHandler = (activeBtn) => {
     document.getElementById('sideBtnKnives').classList.remove('active-side')
     document.getElementById('sideBtnPistols').classList.remove('active-side')
     document.getElementById('sideBtnRifles').classList.remove('active-side')
-    document.getElementById('sideBtnSniperRifles').classList.remove('active-side')
     document.getElementById('sideBtnPPs').classList.remove('active-side')
     document.getElementById('sideBtnShotguns').classList.remove('active-side')
-    document.getElementById('sideBtnP').classList.remove('active-side')
     
     // add active background
     document.getElementById(activeBtn).classList.add('active-side')
 }
   
 const showDefaults = (type) => {
-
-    let defaultWeapons = {
-        'knives': [
-            'css',
-            'bayonet',
-            'flip',
-            'gut',
-            'karambit',
-            'm9_bayonet',
-            'tactical',
-            'butterfly',
-            'falchion',
-            'push',
-            'survival_bowie',
-            'ursus',
-            'gypsy_jackknife',
-            'stiletto',
-            'widowmaker',
-            'canis',
-            'cord',
-            'skeleton',
-            'outdoor'
-        ],
-        'pistols': [
-            'cz75a',
-            'deagle',
-            'elite',
-            'fiveseven',
-            'glock',
-            'hkp2000',
-            'p250',
-            'revolver',
-            'tec9',
-            'usp_silencer'
-        ],
-        'rifles': [
-            'ak47',
-            'aug',
-            'famas',
-            'galilar',
-            'm4a1_silencer',
-            'm4a1',
-            'sg556'
-        ],
-        'sniperRifles': [
-                'awp',
-                'g3sg1',
-                'scar20',
-                'ssg08'
-        ],
-        'smg': [
-            'mac10',
-            'mp5sd',
-            'mp7',
-            'mp9',
-            'bizon',
-            'p90',
-            'ump45'
-        ],
-        'shotguns': [
-            'xm1014',
-            'nova',
-            'sawedoff',
-            'mag7'
-        ],
-        'mg': [
-            'negev',
-            'm249'
-        ]
-    }
-
-
     // clear main container
     document.getElementById('skinsContainer').innerHTML = ''
 
-    // ALERT!!!
-    // THE AMOUNT OF SHITCODE BELOW IS INSANE
+    // STILL INSANE AMOUNT OF SHITCODE BELOW
 
-    if (type == 'knives') {
+    if (type == 'sfui_invpanel_filter_melee') {
         getJSON(`https://bymykel.github.io/CSGO-API/api/${lang}/skins.json`, (err, res) => {
-            // var to get rid of second m9 bayonet
-            let dublicate = 0
-
             // make unique object for weapons
             const unique = res.filter((obj, index) => {
                 return index === res.findIndex(o => obj.weapon.id === o.weapon.id);
@@ -103,119 +25,73 @@ const showDefaults = (type) => {
             console.log(unique)
 
             unique.forEach(element => {
+                if (element.id.includes('vanilla')) {
+                    let card = document.createElement('div')
+                    card.classList.add('col-3', 'p-2')
 
-                // show only weapons that are selected in defaultWeapons
-                defaultWeapons[type].forEach(weapontype => {
-
-                    if (element.id.includes(weapontype) && element.id.includes('vanilla')) {
-                        if (dublicate < 1 && element.id == "skin-vanilla-weapon_knife_m9_bayonet") {
-
-                        
-                            let card = document.createElement('div')
-                            card.classList.add('col-3', 'p-2')
-
-                            element.weapon.id = `weapon_knife_m9_${weapontype}`
-                                    
-                            // check if knife is selected
-                            let active = ''
-                            if (element.weapon.id == selectedKnife.knife) {
-                                active = 'active-card'
-                            }
-
-                            card.innerHTML = `
-                            <div class="rounded-3 d-flex flex-column card-common weapon-card ${active} weapon_knife" id="${element.weapon.id}">
-                            <a onclick="changeKnife(\'${element.weapon.id}\', ${selectedKnife.steamid})" class="text-decoration-none d-flex flex-column" style="z-index: 0;">
-                                    <img src="${element.image}" class="weapon-img mx-auto my-3" loading="lazy" alt="${element.name}">
-                                    
-                                    <p class="m-0 text-light weapon-skin-title mx-auto text-center">${element.weapon.name}</p>
-                            </a>
-                            <button onclick="knifeSkins(\'${element.weapon.id}\')" class="btn btn-primary text-warning mx-auto my-2" style="z-index: 1;"><small>${langObject.changeSkin}</small></button>
-                            </div>
-                            `
-
-                            document.getElementById('skinsContainer').appendChild(card)
-                        } else if (element.id != "skin-vanilla-weapon_knife_m9_bayonet") {
-                            let card = document.createElement('div')
-                            card.classList.add('col-3', 'p-2')
-
-
-                            element.weapon.id = `weapon_knife_${weapontype}`
-
-                            // check if knife is selected
-                            let active = ''
-                            if (element.weapon.id == selectedKnife.knife) {
-                                active = 'active-card'
-                            }
-
-                            card.innerHTML = `
-                            <div class="rounded-3 d-flex flex-column card-common weapon-card ${active} weapon_knife" id="${element.weapon.id}">
-                            <a onclick="changeKnife(\'${element.weapon.id}\', ${selectedKnife.steamid})" class="text-decoration-none d-flex flex-column" style="z-index: 0;">
-                                    <img src="${element.image}" class="weapon-img mx-auto my-3" loading="lazy" alt="${element.name}">
-                                    
-                                    <p class="m-0 text-light weapon-skin-title mx-auto text-center">${element.weapon.name}</p>
-                            </a>
-                            <button onclick="knifeSkins(\'${element.weapon.id}\')" class="btn btn-primary text-warning mx-auto my-2" style="z-index: 1;"><small>${langObject.changeSkin}</small></button>
-                            </div>
-                            `
-
-                            document.getElementById('skinsContainer').appendChild(card)
-                        }   
-
-                        if (element.id == "skin-vanilla-weapon_knife_m9_bayonet") { dublicate++ }      
+                    // check if knife is selected
+                    let active = ''
+                    if (element.weapon.id == selectedKnife.knife) {
+                        active = 'active-card'
                     }
-                });
 
-            });
+                    element.weapon.id = element.id.slice(13)
+
+                    card.innerHTML = `
+                    <div class="rounded-3 d-flex flex-column card-common weapon-card ${active} weapon_knife" id="${element.weapon.id}">
+                    <a onclick="changeKnife(\'${element.weapon.id}\', ${selectedKnife.steamid})" class="text-decoration-none d-flex flex-column" style="z-index: 0;">
+                            <img src="${element.image}" class="weapon-img mx-auto my-3" loading="lazy" alt="${element.name}">
+                            
+                            <p class="m-0 text-light weapon-skin-title mx-auto text-center">${element.weapon.name}</p>
+                    </a>
+                    <button onclick="knifeSkins(\'${element.weapon.id}\')" class="btn btn-primary text-warning mx-auto my-2" style="z-index: 1;"><small>${langObject.changeSkin}</small></button>
+                    </div>
+                    `
+
+                    document.getElementById('skinsContainer').appendChild(card)      
+                }
+            })
         })
     } else {
         getJSON(`https://bymykel.github.io/CSGO-API/api/${lang}/skins.json`, (err, res) => {
-            // var to get rid of second m4a1-s
-            let dublicate = 0
-
-            // make unique object for defaults
+            // make unique object for weapons
             const unique = res.filter((obj, index) => {
-                return index === res.findIndex(o => obj.weapon.name === o.weapon.name);
-            });
+                return index === res.findIndex(o => obj.weapon.id === o.weapon.id);
+            })
+            console.log(unique)
 
             unique.forEach(element => {
+                if (element.category.id == type) {
+                    let card = document.createElement('div')
+                    card.classList.add('col-3', 'p-2')
 
-                // show only weapons that are selected in defaultWeapons
-                defaultWeapons[type].forEach(weapontype => {
-
-                    if (element.weapon.id.includes(weapontype)) {                   
-                        if (dublicate < 1) {
-                            let card = document.createElement('div')
-                            card.classList.add('col-3', 'p-2')
-
-                            card.innerHTML = `
-                            <div class="rounded-3 d-flex flex-column card-common weapon-card weapon_knife" id="${element.weapon.id}">
-                            <a class="text-decoration-none d-flex flex-column" style="z-index: 0;">
-                                    <img src="${element.image}" class="weapon-img mx-auto my-3" loading="lazy" alt="${element.name}">
-                                    
-                                    <p class="m-0 text-light weapon-skin-title mx-auto text-center">${element.weapon.name}</p>
-                            </a>
-                            <button onclick="knifeSkins(\'${element.weapon.id}\')" class="btn btn-primary text-warning mx-auto my-2" style="z-index: 1;"><small>${langObject.changeSkin}</small></button>
-                            </div>
-                            `
-
-                            document.getElementById('skinsContainer').appendChild(card)
-                        }
-
-                        // get rid of second m4a1-s
-                        if (element.weapon.id == 'weapon_m4a1_silencer') { dublicate++ }        
+                    // check if knife is selected
+                    let active = ''
+                    if (element.weapon.id == selectedKnife.knife) {
+                        active = 'active-card'
                     }
 
-                });
+                    card.innerHTML = `
+                    <div class="rounded-3 d-flex flex-column card-common weapon-card ${active} weapon_knife" id="${element.weapon.id}">
+                    <a class="text-decoration-none d-flex flex-column" style="z-index: 0;">
+                            <img src="${element.image}" class="weapon-img mx-auto my-3" loading="lazy" alt="${element.name}">
+                            
+                            <p class="m-0 text-light weapon-skin-title mx-auto text-center">${element.weapon.name}</p>
+                    </a>
+                    <button onclick="knifeSkins(\'${element.weapon.id}\')" class="btn btn-primary text-warning mx-auto my-2" style="z-index: 1;"><small>${langObject.changeSkin}</small></button>
+                    </div>
+                    `
 
-            });
+                    document.getElementById('skinsContainer').appendChild(card)      
+                }
+            })
         })
     }
-    
 }
 
 const showKnives = () => {
     sideBtnHandler('sideBtnKnives')
-    showDefaults('knives')
+    showDefaults('sfui_invpanel_filter_melee')
 }
 
 const showGloves = () => {
@@ -224,32 +100,32 @@ const showGloves = () => {
 
 const showPistols = () => {
     sideBtnHandler('sideBtnPistols')
-    showDefaults('pistols')
+    showDefaults('csgo_inventory_weapon_category_pistols')
 }
 
 const showRifles = () => {
     sideBtnHandler('sideBtnRifles')
-    showDefaults('rifles')
+    showDefaults('csgo_inventory_weapon_category_rifles')
 }
 
 const showSniperRifles = () => {
     sideBtnHandler('sideBtnSniperRifles')
-    showDefaults('sniperRifles')
+    showDefaults('csgo_inventory_weapon_category_rifles')
 }
 
 const showPPs = () => {
     sideBtnHandler('sideBtnPPs')
-    showDefaults('smg')
+    showDefaults('csgo_inventory_weapon_category_smgs')
 }
 
 const showShotguns = () => {
     sideBtnHandler('sideBtnShotguns')
-    showDefaults('shotguns')
+    showDefaults('csgo_inventory_weapon_category_heavy')
 }
 
 const showP = () => {
     sideBtnHandler('sideBtnP')
-    showDefaults('mg')
+    showDefaults('csgo_inventory_weapon_category_heavy')
 }
 
 const changeKnife = (weaponid) => {
