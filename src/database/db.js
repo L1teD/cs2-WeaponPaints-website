547e96b676d0b8b9d92682a6481351f25e7ffb49
Daniil Klimchuk <89979325+L1teD@users.noreply.github.com>
2024-08-12 18:33:02 +0900

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -4,13 +4,12 @@ const config = require('../../config.json');
 const connection = mysql.createConnection(config.DB);
 
 async function query(sql, params) {
-    let connection = mysql.createPool(config.DB)
     try {
         let results = await connection.query(sql)
         results = results[0]
         return results;
     } finally {
-        connection.releaseConnection();
+        connection.destroy();
     }  
 }
 
